12645d858ae8a36d55992e6b9291d486cdaede33
Julian-Azange <julianazange@gmail.com>
2024-12-01 00:48:57 -0500

    MOD: pagina principal full responsiva

--- src/page/flightSearch/FlightSearch.jsx
+++ src/page/flightSearch/FlightSearch.jsx
@@ -1,42 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import logo from '../../assets/logo.png';
-import { Form, Button, InputGroup, FormControl, Container, Modal } from 'react-bootstrap';
-import { MdFlightTakeoff, MdFlightLand, MdDateRange, MdGroup } from 'react-icons/md';
+import { Form, Button, InputGroup, FormControl, Container, Modal, Card, Row, Col } from 'react-bootstrap';
+import { MdFlightTakeoff, MdFlightLand, MdDateRange, MdSearch, MdRemove, MdAdd } from 'react-icons/md';
 import './flightSearch.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useStoreFlight } from '../../store/store'
-import CardSearch from "../../components/CardSearch"
-import caliImg from "../../assets/cali.jpg"
-import medellinImg from "../../assets/medellin.jpg"
-import { useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom";
-
-const cards = [{
-    "city": "Cali",
-    "description": "Sumérgete en la cultura y la energía de Cali con precios especiales para tu viaje.",
-    "discount": "7",
-    "pathImg": caliImg
-},
-{
-    "city": "Medellin",
-    "description": "Explora la ciudad de la eterna primavera con increíbles ofertas y paquetes especiales.",
-    "discount": "10",
-    "pathImg": medellinImg
-},
-]
+import { useNavigate, Link } from 'react-router-dom';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
 
 const FlightSearch = () => {
-
     const navigate = useNavigate();
     const [airports, setAirports] = useState([]);
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
-    const [date, setDate] = useState('');
+    const [date, setDate] = useState(null); // Date state
     const [passengers, setPassengers] = useState({ adults: 1, children: 0, babies: 0 }); // Pasajeros
     const [showModal, setShowModal] = useState(false); // Estado para el modal
+    const [showCalendar, setShowCalendar] = useState(false); // Estado para mostrar el calendario
     const { start_flight } = useStoreFlight();
+    const inputRef = useRef();
 
+    // Fetch airports on mount
+    useEffect(() => {
+        const fetchAirports = async () => {
+            try {
+                start_flight();
+                const response = await axios.get('https://cantozil.pythonanywhere.com/api/airports/');
+                setAirports(response.data);
+            } catch (err) {
+                console.error('Error fetching airports:', err);
+            }
+        };
+        fetchAirports();
+    }, [start_flight]);
+
+    // Handle passengers modal
     const handlePassengersChange = (type, operation) => {
         setPassengers((prev) => {
             let value = operation === 'increase' ? prev[type] + 1 : prev[type] - 1;
@@ -56,12 +57,38 @@ const FlightSearch = () => {
         });
     };
 
+    // Handle date selection
+    const handleDateSelect = (selectedDate) => {
+        setDate(selectedDate);
+        setShowCalendar(false);
+    };
+
+    // Handle input click to toggle calendar
+    const handleInputClick = () => {
+        setShowCalendar(!showCalendar);
+    };
+
+    // Close calendar if clicked outside
+    const handleClickOutside = (event) => {
+        if (inputRef.current && !inputRef.current.contains(event.target)) {
+            setShowCalendar(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    // Handle search submission
     const handleSubmit = (e) => {
         e.preventDefault();
         const searchParams = new URLSearchParams({
             origin: origin,
             destination: destination,
-            date: date,
+            date: date ? format(date, 'yyyy-MM-dd') : '',
             adults: passengers.adults,
             children: passengers.children,
             babies: passengers.babies
@@ -69,147 +96,145 @@ const FlightSearch = () => {
         navigate(`/flightList?${searchParams.toString()}`);
     };
 
-    useEffect(() => {
-        const fetchAirports = async () => {
-            try {
-                start_flight();
-                const response = await axios.get('https://cantozil.pythonanywhere.com/api/airports/');
-                setAirports(response.data);
-            } catch (err) {
-                console.error('Error fetching airports:', err);
-            }
-        };
-        fetchAirports();
-    }, [start_flight]);
-
     return (
         <>
-            <header className="header mb-4">
+            <header className="header m-4 d-flex justify-content-between align-items-center px-4">
                 <Link to="/">
-                    <img
-                        src={logo}
-                        width="150"
-                        height="60"
-                        alt="Logo Portal de Pago Air"
-                        className="logo-left"
-                    />
+                    <img src={logo} width="100" alt="Logo" className="logo-left" />
                 </Link>
             </header>
-            <div className="hero-section">
-                <Container>
-                    <div className="hero-content">
-                        <h1 className="mb-4">Explora el mundo y disfruta de su belleza</h1>
-                        <p className="mb-5">Encuentra y comparte tus experiencias alrededor del mundo.</p>
-                    </div>
-                    <div className="search-card">
-                        <Form onSubmit={handleSubmit} className="w-100">
-                            <div className="d-flex flex-wrap gap-3 justify-content-center">
-                                <InputGroup className="mb-3" style={{ maxWidth: '260px' }}>
-                                    <InputGroup.Text>
-                                        <MdFlightTakeoff />
-                                    </InputGroup.Text>
-                                    <Form.Select
-                                        placeholder="Origen"
-                                        value={origin}
-                                        onChange={(e) => setOrigin(e.target.value)}
-                                        required
-                                    >
-                                        <option disabled value="">Selecciona Origen</option>
-                                        {airports.map((airport) => (
-                                            <option key={airport.code} value={airport.code}>
-                                                {airport.name} - {airport.code}
+
+            <div className="hero-section d-flex align-items-end" style={{ backgroundSize: 'cover', padding: '5rem 0', color: '#fff', minHeight: '70vh', position: 'relative' }}>
+                <Container className="text-center" style={{ marginBottom: '3rem' }}>
+                    <h1 className="display-4 fw-medium mb-2">Explora el mundo y disfruta de su belleza</h1>
+                    <h4 className="mb-5">Encuentra y comparte tus experiencias alrededor del mundo</h4>
+                    <Card className="p-4 mx-auto" style={{ maxWidth: '900px', background: 'rgba(255, 255, 255, 0.9)', borderRadius: '1rem' }}>
+                        <Form onSubmit={handleSubmit}>
+                            <Row className="g-3">
+                                <Col xs={12} md={3}>
+                                    <InputGroup>
+                                        <InputGroup.Text>
+                                            <MdFlightTakeoff />
+                                        </InputGroup.Text>
+                                        <Form.Select
+                                            aria-label="Origen"
+                                            value={origin}
+                                            onChange={(e) => setOrigin(e.target.value)}
+                                            required
+                                        >
+                                            <option disabled value="">
+                                                Origen
                                             </option>
-                                        ))}
-                                    </Form.Select>
-                                </InputGroup>
-
-                                <InputGroup className="mb-3" style={{ maxWidth: '260px' }}>
-                                    <InputGroup.Text>
-                                        <MdFlightLand />
-                                    </InputGroup.Text>
-                                    <Form.Select
-                                        placeholder="Destino"
-                                        value={destination}
-                                        onChange={(e) => setDestination(e.target.value)}
-                                        required
-                                    >
-                                        <option disabled value="">Selecciona Destino</option>
-                                        {airports.map((airport) => (
-                                            <option key={airport.code} value={airport.code}>
-                                                {airport.name} - {airport.code}
+                                            {airports.map((airport) => (
+                                                <option key={airport.code} value={airport.code}>
+                                                    {airport.name} - {airport.code}
+                                                </option>
+                                            ))}
+                                        </Form.Select>
+                                    </InputGroup>
+                                </Col>
+                                <Col xs={12} md={3}>
+                                    <InputGroup>
+                                        <InputGroup.Text>
+                                            <MdFlightLand />
+                                        </InputGroup.Text>
+                                        <Form.Select
+                                            aria-label="Destino"
+                                            value={destination}
+                                            onChange={(e) => setDestination(e.target.value)}
+                                            required
+                                        >
+                                            <option value="" disabled>
+                                                Destino
                                             </option>
-                                        ))}
-                                    </Form.Select>
-                                </InputGroup>
+                                            {airports.map((airport) => (
+                                                <option key={airport.code} value={airport.code}>
+                                                    {airport.name} - {airport.code}
+                                                </option>
+                                            ))}
+                                        </Form.Select>
+                                    </InputGroup>
+                                </Col>
+                                <Col xs={12} md={3}>
+                                    <div ref={inputRef} className="position-relative">
+                                        <InputGroup onClick={handleInputClick}>
+                                            <InputGroup.Text>
+                                                <MdDateRange />
+                                            </InputGroup.Text>
+                                            <FormControl
+                                                type="text"
+                                                value={date ? format(date, 'dd/MM/yyyy') : ''}
+                                                placeholder="Selecciona fecha"
+                                                readOnly
+                                                required
+                                            />
+                                        </InputGroup>
 
-                                <InputGroup className="mb-3" style={{ maxWidth: '200px' }}>
-                                    <InputGroup.Text>
-                                        <MdDateRange />
-                                    </InputGroup.Text>
-                                    <FormControl
-                                        type="date"
-                                        value={date}
-                                        onChange={(e) => setDate(e.target.value)}
-                                        required
-                                    />
-                                </InputGroup>
-
-                                <Button variant="secondary" onClick={() => setShowModal(true)} className="mb-3">
-                                    <MdGroup /> {passengers.adults + passengers.children + passengers.babies} pasajeros
-                                </Button>
-
-                                <Button
-                                    type="submit"
-                                    className="mb-3 btn-search"
-                                >
-                                    Buscar
-                                </Button>
-                            </div>
+                                        {showCalendar && (
+                                            <div className="day-picker-popup position-absolute">
+                                                <DayPicker
+                                                    mode="single"
+                                                    selected={date}
+                                                    onSelect={handleDateSelect}
+                                                    fromDate={new Date()} // Solo permitir seleccionar desde la fecha actual
+                                                    styles={{
+                                                        today: { backgroundColor: '#349dd4', color: '#ffffff' },
+                                                        selected: { backgroundColor: '#0e3b5e', color: '#ffffff' },
+                                                    }}
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                </Col>
+                                <Col xs={12} md={2}>
+                                    <Button className="btn-passenger w-100" onClick={() => setShowModal(true)}>
+                                        {passengers.adults + passengers.children + passengers.babies} Pasajeros
+                                    </Button>
+                                </Col>
+                                <Col xs={12} md={1} className="d-flex align-items-center">
+                                    <Button type="submit" className="btn-search w-100">
+                                        <MdSearch />
+                                    </Button>
+                                </Col>
+                            </Row>
                         </Form>
-                    </div>
+                    </Card>
                 </Container>
             </div>
 
-            <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <Modal show={showModal} onHide={() => setShowModal(false)} centered>
                 <Modal.Header closeButton>
                     <Modal.Title>¿Quiénes vuelan?</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    {["adults", "children", "babies"].map((type, index) => (
-                        <div key={index} className="d-flex justify-content-between align-items-center mb-3">
-                            <span>
-                                {type === "adults" ? "Adultos" : type === "children" ? "Niños" : "Bebés"}
-                            </span>
-                            <div className="d-flex align-items-center gap-2">
-                                <Button
-                                    variant="outline-secondary"
-                                    onClick={() => handlePassengersChange(type, "decrease")}
-                                >
-                                    -
+                    {["adults", "children", "babies"].map((type) => (
+                        <div key={type} className="d-flex justify-content-between align-items-center mb-3">
+                            <div>
+                                <h6 className="mb-0 text-capitalize">
+                                    {type === "adults" ? "Adultos" : type === "children" ? "Niños" : "Bebés"}
+                                </h6>
+                                <small className="text-muted">
+                                    {type === 'adults' && 'Desde 15 años'}
+                                    {type === 'children' && 'Menores de 12 años'}
+                                    {type === 'babies' && 'Menores de 2 años'}
+                                </small>
+                            </div>
+                            <div className="d-flex align-items-center">
+                                <Button variant="outline-secondary" size="sm" onClick={() => handlePassengersChange(type, 'decrease')} disabled={passengers[type] === 0}>
+                                    <MdRemove />
                                 </Button>
-                                <span>{passengers[type]}</span>
-                                <Button
-                                    variant="outline-secondary"
-                                    onClick={() => handlePassengersChange(type, "increase")}
-                                >
-                                    +
+                                <span className="mx-3 fs-5">{passengers[type]}</span>
+                                <Button variant="outline-secondary" size="sm" onClick={() => handlePassengersChange(type, 'increase')}>
+                                    <MdAdd />
                                 </Button>
                             </div>
                         </div>
                     ))}
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={() => setShowModal(false)}>
+                    <Button variant="" className="w-100 bg-cyan text-white fw-medium" onClick={() => setShowModal(false)}>
                         Confirmar
                     </Button>
-                </Modal.Footer>
+                </Modal.Body>
             </Modal>
-
-            <div className="containerCards">
-                {cards.map((item) => (
-                    <CardSearch key={item.city} data={item} />
-                ))}
-            </div>
         </>
     );
 };
